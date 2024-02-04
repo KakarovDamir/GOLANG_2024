c5bb41df12dd2c54ee79fe11b156cd3cef721d30
Damir <kdamir2004@gmail.com>
2024-02-04 23:35:06 +0600

    tsis1

--- tsis1/pkg/porsche-api/model.go
+++ tsis1/pkg/porsche-api/model.go
@@ -5,48 +5,48 @@ import (
 )
 
 type Car struct {
-	ID        string   `json:"id"`
-	Brand     string `json:"brand"`
-	Model     string `json:"model"`
-	Year      string   `json:"year"`
+	ID    string `json:"id"`
+	Brand string `json:"brand"`
+	Model string `json:"model"`
+	Year  string `json:"year"`
 }
 
 var cars = []Car{
 	{
-		ID:      "1",
-		Brand:   "Porsche",
-		Model:   "911 Turbo S",
-		Year:    "2022",
+		ID:    "1",
+		Brand: "Porsche",
+		Model: "911 Turbo S",
+		Year:  "2022",
 	},
 	{
-		ID:      "2",
-		Brand:   "Porsche",
-		Model:   "Cayman GT4",
-		Year:    "2023",
+		ID:    "2",
+		Brand: "Porsche",
+		Model: "Cayman GT4",
+		Year:  "2023",
 	},
 	{
-		ID:      "3",
-		Brand:   "Porsche",
-		Model:   "Panamera Turbo S E-Hybrid",
-		Year:    "2022",
+		ID:    "3",
+		Brand: "Porsche",
+		Model: "Panamera Turbo S E-Hybrid",
+		Year:  "2022",
 	},
 	{
-		ID:      "4",
-		Brand:   "Porsche",
-		Model:   "Cayenne",
-		Year:    "2022",
+		ID:    "4",
+		Brand: "Porsche",
+		Model: "Cayenne",
+		Year:  "2022",
 	},
 	{
-		ID:      "5",
-		Brand:   "Porsche",
-		Model:   "Macan GTS",
-		Year:    "2023",
+		ID:    "5",
+		Brand: "Porsche",
+		Model: "Macan GTS",
+		Year:  "2023",
 	},
 	{
-		ID:      "6",
-		Brand:   "Porsche",
-		Model:   "Boxstar",
-		Year:    "2022",
+		ID:    "6",
+		Brand: "Porsche",
+		Model: "Boxstar",
+		Year:  "2022",
 	},
 }
 
@@ -55,10 +55,10 @@ func GetCars() []Car {
 }
 
 func GetCar(id string) (*Car, error) {
-	for _, r := range cars {
-		if r.ID == id {
-			return &r, nil
-		}
+	for i := 0; i < len(cars); i++ {
+		if cars[i].ID == id {
+			return &cars[i], nil
+		} 
 	}
 	return nil, errors.New("porsche not found")
 }
